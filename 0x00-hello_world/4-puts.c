8664eaf08cdc9ce7596cb96aa612cda1ccea0557
poojadoshi12 <pooja.doshi44@gmail.com>
2022-07-05 11:38:40 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
+#include <stdio.h>
 /**
  * main-entry 
+ *
  * return: 0after printing
  */ 
-#include <stdio.h>
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
 /**
  * main -entry 
  * return 
  */
-#include <stdio.h>
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
