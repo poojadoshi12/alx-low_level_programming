c0f2e7bbe1b238169e2a7ca78af3762c4f9a6d18
poojadoshi12 <pooja.doshi44@gmail.com>
2022-07-12 03:50:00 -0700

    55

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev -> printing
+ * @s: the string to print
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
