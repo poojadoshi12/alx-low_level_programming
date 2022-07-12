bdc660173e0c52ac1ade3511bcfb839d124e16ce
poojadoshi12 <pooja.doshi44@gmail.com>
2022-07-12 04:09:29 -0700

    6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,7 +6,6 @@
  */
 void rev_string(char *s)
 {
-	"sam"->"mas"
 	char tmp;
 	int i, length1, length2;
 
@@ -15,6 +14,7 @@ void rev_string(char *s)
 
 	while (s[length1] != '\0')
 		length1++;
+
 	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
